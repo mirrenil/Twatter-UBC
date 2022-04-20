083ee0fb3d676a472d4704b93cb57d01e1e27b1e
linnea <linnea.albertsson@gmail.com>
2022-04-20 15:01:11 +0200

    Created PostComponent

---
+++ client/components/PostComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const PostComponent = () => {
+    return (
+        <div style={{height: "20rem", width: "20rem"}}>HELLOOOOOOOOOOO00000000000000</div>
+    )
+}
