7ec75169c1acc2b0dae82ae5f2d6b2c89b4f04c0
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-04-26 15:36:04 +0200

    adjusted the state of logged in and signed in

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,19 @@
 import { CSSProperties, useState } from "react";
 import LogInComponent from "./LogInComponent";
-import SignUp from "./SignUpComponent";
+import SignUpComponent from "./SignUpComponent";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState<Boolean>(false);
+    const [isLogInOpen, setIsLogInOpen] = useState<Boolean>(false);
+    const [isSignedInOpen, setIsSignedInOpen] = useState<Boolean>(false);
+
     return <div className="header">
         <h1>Twatter</h1>
         <input className="search" type="search" name="search" id="search" placeholder="Search..."/>
-        <button style={btn} onClick={() => setIsOpen(true)}>Sign In</button>
-      <LogInComponent open={isOpen} onClose={() => setIsOpen(false)} />
+        <button style={btn} onClick={() => setIsLogInOpen(true)}>Sign In</button>
+      <LogInComponent open={isLogInOpen} onClose={() => setIsLogInOpen(false)} />
       <div>
-       <button style={btn} onClick={() => setIsOpen(true)}>Sign Up</button>
-      <SignUp open={isOpen} onClose={() => setIsOpen(false)} />
+       <button style={btn} onClick={() => setIsSignedInOpen(true)}>Sign Up</button>
+      <SignUpComponent open={isSignedInOpen} onClose={() => setIsSignedInOpen(false)} />
       </div>
      
     </div>
