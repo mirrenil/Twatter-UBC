d52b80382646391d3a3efcafb7cfb5f025212a97
Miranda Nilhag <90900256+mirrenil@users.noreply.github.com>
2022-04-27 18:10:19 +0200

    Removed the footer

--- client/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    console.log('footer is running');
-    return (
-        <footer className="footer">
-            <div>I'm a footer</div>
-        </footer>
-    );
-}
-export default Footer;
--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout = () => {
   window.scrollTo(0, 0);
 
-  console.log('layyyyout')
   return (
     <div>
       <Header />
       <Outlet />
-      <Footer />
     </div>
   );
 };
