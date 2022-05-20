0260b39263e29d6eb8e68f59f8191571c2fe4e15
linnea <linnea.albertsson@gmail.com>
2022-05-20 16:47:30 +0200

    fixed wallpost PUT

--- server/routes/wallPostRoutes.js
+++ server/routes/wallPostRoutes.js
@@ -10,16 +10,23 @@ router.use(Express.json());
 router.get('/wallposts', async (req, res) => {
   try {
     const wallPosts = await wallPostModel.find({});
+    if(!wallPosts.lenght) {
+      return res.status(404).json(wallPosts);
+    }
     res.json(wallPosts);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-/** ---- GET ONE POST----- */
-router.get('/wallposts/:user', async (req, res) => {
+/** ---- GET ONE POST ----- */
+router.get('/wallpost/:id', async (req, res) => {
+  const { id } = req.params;
   try {
-    const wallPost = await wallPostModel.findOne({ user: req.params.user });
+    const wallPost = await wallPostModel.findById(id);
+    if (!wallPost) {
+      return res.status(404).json(wallPost);
+    }
     res.json(wallPost);
   } catch (err) {
     res.status(400).json(err);
@@ -43,7 +50,7 @@ router.post('/wallposts/newpost', async (req, res) => {
       return res.status(201).json(newPost);
     } catch (err) {
       return res
-        .status(400)
+        .status(418)
         .json(
           'Why post something someone else has already posted? Be original, pls...'
         );
@@ -58,17 +65,20 @@ router.put('/wallposts/:id', async (req, res) => {
   const { id } = req.params;
   const currentPost = await wallPostModel.findById(id);
   const loggedInUser = req.session.user;
+  console.log('in put wallpost')
 
-  if (!loggedInUser) {
-    return res
-      .status(403)
-      .json('Nooooo stop!! You have to sign in first...stupid');
-  } else if (!currentPost) {
-    return res.status(400).json('no post found');
-  }
+  try {
+    console.log('in try')
+    if (!loggedInUser) {
+      return res.status(403).json('Nooooo stop!! You have to sign in first...stupid');
+    } else if (!currentPost) {
+      return res.status(404).json('no post found');
+    }
+    console.log(currentPost.username)
+    console.log(req.session.user.username)
 
-  if (currentPost.username === req.session.user.username) {
-    try {
+    if (currentPost.username === req.session.user.username) {
+      console.log('currentpost username stämmmer med req session')
       const wallPost = await wallPostModel.findByIdAndUpdate(id, req.body, {
         useFindAndModify: false,
       });
@@ -79,9 +89,11 @@ router.put('/wallposts/:id', async (req, res) => {
           new: req.body,
         })
         .status(200);
-    } catch (err) {
-      return res.status(400).json('An error occured');
+    } else {
+      return res.status(403).json('u cant edit other peoples posts just like that... ur mean')
     }
+  } catch (err) {
+    return res.status(400).json(err);
   }
 });
 
@@ -96,7 +108,7 @@ router.delete('/wallposts/:id', async (req, res) => {
   if (!loggedInUser) {
     return res.status(401).json('You must sign in first');
   } else if (!currentPost) {
-    return res.status(400).json('no post found');
+    return res.status(404).json('no post found');
   }
 
   if (currentPost.username === req.session.user.username) {
@@ -108,7 +120,7 @@ router.delete('/wallposts/:id', async (req, res) => {
         return res.status(200).json('post removed');
       }
     } catch (err) {
-      return res.status(400).json('An error occured');
+      return res.status(400).json(err);
     }
   } else {
     res.status(403).json('U obviously cant delete someone elses post');
